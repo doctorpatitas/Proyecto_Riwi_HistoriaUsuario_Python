b74704f8c10e361081f37b6108d89b53fc1c475b
doctorpatitas <diegogonzal272111@gmail.com>
2026-03-20 20:27:45 -0500

    Se ha agregado el objetivo de esta semana al momento de crear la historia de usuario 2.

---
+++ objetivo_semana.py
@@ -0,0 +1,3 @@
+"""El objetivo de la historia de usuario de esta semana fue aprender a usar 
+condicionales, ciclos, listas, diccionarios, funciones, aprender a modularizar funciones
+y aprender a comentar y documentar codigo de manera mas extensa."""

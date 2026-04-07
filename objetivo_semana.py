f4f95a0201711cb644a7a9658609fcd270c28ef2
Doctor_patitas <diegogonzal272111@gmail.com>
2026-04-06 23:11:26 -0500

    Delete objetivo_semana.py

--- objetivo_semana.py
+++
@@ -1,3 +0,0 @@
-"""El objetivo de la historia de usuario de esta semana fue aprender a usar 
-condicionales, ciclos, listas, diccionarios, funciones, aprender a modularizar funciones
-y aprender a comentar y documentar codigo de manera mas extensa."""
